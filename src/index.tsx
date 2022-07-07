a81f7d8eff44487449c852c0807fecc59b313e8e
Artembabych3377 <artembabych3377@gmail.com>
2022-07-07 20:42:23 +0300

    added pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { SnackbarProvider } from './components/SnackbarContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
       <SnackbarProvider>
         <App />
       </SnackbarProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
